87bd800270aa79c0ada0de322baf19e9d817ea7f
ghulammurtaza786 <murtazaghulam345@gmail.com>
2026-06-04 01:58:43 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+const rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    rooms[roomId].push(socket.id);
+
+    // notify others
+    socket.to(roomId).emit("user-joined", socket.id);
+
+    // send existing users
+    socket.emit("existing-users", rooms[roomId].filter(id => id !== socket.id));
+
+    socket.on("disconnect", () => {
+      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      socket.to(roomId).emit("user-left", socket.id);
+    });
+  });
+
+  socket.on("offer", (data) => {
+    io.to(data.to).emit("offer", {
+      from: socket.id,
+      offer: data.offer
+    });
+  });
+
+  socket.on("answer", (data) => {
+    io.to(data.to).emit("answer", {
+      from: socket.id,
+      answer: data.answer
+    });
+  });
+
+  socket.on("ice-candidate", (data) => {
+    io.to(data.to).emit("ice-candidate", {
+      from: socket.id,
+      candidate: data.candidate
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
